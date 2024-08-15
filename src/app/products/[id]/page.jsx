9f0b1ef95467173340235de48eaa54219e216c21
theiguim <theiguim@gmail.com>
2024-08-14 21:46:20 -0300

    versão final

---
+++ src/app/products/[id]/page.jsx
@@ -0,0 +1,40 @@
+import ProductDetails from "@/components/ProductDetails";
+import { fetchProduct, fetchProducts } from "@/services/products";
+import { Container } from "reactstrap";
+
+
+export const generateStaticParams = async () => {
+
+    const products = await fetchProducts();
+
+    return products.map(product => ({
+        id: product.id.toString()
+    }))
+};
+
+export const generateMetadata = async ({ params }) => {
+    const { id } = params;
+    const product = await fetchProduct(id);
+    return {
+        title: product.name,
+        description: product.description,
+        icons: {
+            icon: "/favicon.ico"
+        }
+    }
+}
+
+
+const Product = async ({ params }) => {
+
+    const { id } = params;
+    const product = await fetchProduct(id)
+
+    return (
+        <Container className="mt-5">
+            <ProductDetails product={product} />
+        </Container>
+    )
+};
+
+export default Product
